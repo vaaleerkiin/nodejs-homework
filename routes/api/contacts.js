304654ed3f115333831996f749f83dfe815971db
Valera Kamelkov <tahaburrent@gmail.com>
2023-07-09 15:59:34 +0200

    ~

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,33 +1,29 @@
 const express = require("express");
-
-const {
-  getAll,
-  getById,
-  addContact,
-  deleteContact,
-  editContact,
-} = require("../../controlers/contacts");
-const isValidId = require("../../middlewares/isValidId");
-const validateBody = require("../../middlewares/validateBody");
+const ctrl = require("../../controlers/contacts/");
+const { isValidId, validateBody } = require("../../middlewares");
 const { shema } = require("../../models/contact");
-
 const router = express.Router();
 
-router.get("/", getAll);
+router.get("/", ctrl.getAll);
 
-router.get("/:contactId", isValidId, getById);
+router.get("/:contactId", isValidId, ctrl.getById);
 
-router.post("/", isValidId, validateBody(shema.addShema), addContact);
+router.post("/", validateBody(shema.addShema), ctrl.addContact);
 
-router.delete("/:contactId", deleteContact);
+router.delete("/:contactId", isValidId, ctrl.deleteContact);
 
-router.put("/:contactId", isValidId, validateBody(shema.addShema), editContact);
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(shema.addShema),
+  ctrl.editContact
+);
 
 router.patch(
   "/:contactId/favorite",
   isValidId,
   validateBody(shema.updateFavoriteSchema),
-  editContact
+  ctrl.editContact
 );
 
 module.exports = router;
