6c6ea26144bdfd91ba6f9701764643216b623ec1
Valera Kamelkov <tahaburrent@gmail.com>
2023-07-13 13:43:23 +0200

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,7 +7,7 @@ const Authenticate = async (req, res, next) => {
   try {
     const { authorization = "" } = req.headers;
     const [bearer, token] = authorization.split(" ");
-    if (bearer !== "Bearer") {
+    if (bearer !== "Bearer" || token) {
       throw HttpError(401);
     }
     try {
