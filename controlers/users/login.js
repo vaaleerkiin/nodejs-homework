abdba322a9aa4a57af9589a078466015ee3e6ead
Valera Kamelkov <tahaburrent@gmail.com>
2023-08-04 13:46:05 +0200

    ~

--- controlers/users/login.js
+++ controlers/users/login.js
@@ -8,7 +8,10 @@ const login = async (req, res, next) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
-  if (!user && !user.verify) {
+  // if (!user && !user.verify) {
+  //   throw HttpError(401, "Email or password is wrong");
+  // }
+  if (!user) {
     throw HttpError(401, "Email or password is wrong");
   }
 
